c3e6636dbe427602a7f271912774fe67eeca2c51
Mark Clearwater <csmacnz@csmac.co.nz>
2014-12-02 18:37:44 +1300

    Added Comments
    * GenericExtensions comments
    * TreatWarningsAsErrors turned on

--- src/BCLExtensions/GenericExtensions.cs
+++ src/BCLExtensions/GenericExtensions.cs
@@ -5,8 +5,18 @@ using System.Text;
 
 namespace BCLExtensions
 {
+    /// <summary>
+    /// Extension methods that can be applied Generically.
+    /// </summary>
     public static class GenericExtensions
     {
+        /// <summary>
+        /// Takes a reference type, and returns it, or default if the reference type is null
+        /// </summary>
+        /// <typeparam name="T">Type of the item</typeparam>
+        /// <param name="item">The item to check</param>
+        /// <param name="defaultValue">The default value</param>
+        /// <returns>item if not null; otherwise defaultValue</returns>
         public static T GetValueOrDefault<T>(this T item, T defaultValue) where T : class
         {
             if (defaultValue == null) throw new ArgumentNullException("defaultValue");
