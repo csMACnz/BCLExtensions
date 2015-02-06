6c7b589663145601595edd0230bed017e8434b96
Mark Clearwater <csmacnz@csmac.co.nz>
2015-02-06 18:31:15 +1300

    added extension IsOfType and tests.

---
+++ src/BCLExtensions.Tests/TypeExtensions/IsOfTypeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace BCLExtensions.Tests.TypeExtensions
+{
+    public class IsOfTypeTests
+    {
+        [Fact]
+        public void WhenInstanceIsNullReturnsFalse()
+        {
+            object item = null;
+            var result = item.IsOfType<object>();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void WhenInstanceIsObjectAndOfTypeIsObjectReturnsTrue()
+        {
+            object item = new object();
+            var result = item.IsOfType<object>();
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void WhenInstanceIsStringAndOfTypeIsObjectReturnsFalse()
+        {
+            object item = new String('c',5);
+            var result = item.IsOfType<object>();
+            Assert.False(result);
+        }
+
+    }
+}
---
+++ src/BCLExtensions/TypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BCLExtensions
+{
+    /// <summary>
+    /// Extension methods for the <see cref="System.Type"/> class.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Checks if the value is of the exact type of the Generic type T.
+        /// </summary>
+        /// <param name="value">The value whose type is being checked.</param>
+        /// <returns>true if value is not null and the exact type of value matches T, otherwise false.</returns>
+        public static bool IsOfType<T>(this object value)
+        {
+            if (value == null) return false;
+            return value.GetType() == typeof(T);
+        }
+    }
+}
