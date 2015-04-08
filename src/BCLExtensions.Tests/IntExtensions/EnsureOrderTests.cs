61f9552ffe5b4f35200f9d99a1ec521146a05acf
Mark Clearwater <csmacnz@csmac.co.nz>
2015-04-08 22:00:57 +1200

    Implemented Exclusive/Inclusive integer range check
    * EnsureOrder
    * InclusiveInteger
    * ExclusiveInteger
    * IsBetween X4 combinations

---
+++ src/BCLExtensions.Tests/IntExtensions/EnsureOrderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BCLExtensions.Tests.TestHelpers;
+using Xunit;
+using Xunit.Extensions;
+
+namespace BCLExtensions.Tests.IntExtensions
+{
+    public class EnsureOrderTests
+    {
+        [Theory]
+        [InlineData(int.MaxValue, int.MinValue)]
+        [InlineData(1, 0)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(0, -1)]
+        [InlineData(-1, -2)]
+        [InlineData(-2, -3)]
+        [InlineData(42, -42)]
+        public void InvalidOrderThrowsExceptions(int first, int second)
+        {
+            Action<int, int> ensureOrder = BCLExtensions.IntExtensions.EnsureOrder;
+
+            Assert.Throws<InvalidOperationException>(ensureOrder.AsActionUsing(first, second).AsThrowsDelegate());
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(int.MinValue, int.MaxValue)]
+        [InlineData(0, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        [InlineData(-1, 0)]
+        [InlineData(-2, -1)]
+        [InlineData(-3, -2)]
+        [InlineData(42, 42)]
+        [InlineData(-42, 42)]
+        public void ValidDataDoesNotThrowExceptions(int first, int second)
+        {
+            Action<int, int> ensureOrder = BCLExtensions.IntExtensions.EnsureOrder;
+
+            Assert.DoesNotThrow(ensureOrder.AsActionUsing(first, second).AsThrowsDelegate());
+        }
+    }
+}
--- src/BCLExtensions/IntExtensions.cs
+++ src/BCLExtensions/IntExtensions.cs
@@ -8,6 +8,93 @@ namespace BCLExtensions
     public static class IntExtensions
     {
         /// <summary>
+        /// Ensures the order where first is less than or equal to the second.
+        /// </summary>
+        /// <param name="first">The first number.</param>
+        /// <param name="second">The second number.</param>
+        /// <exception cref="System.InvalidOperationException">The second cannot be less than the first.</exception>
+        public static void EnsureOrder(int first, int second)
+        {
+            if (second < first) throw new InvalidOperationException("The first value {0} cannot be larger than the second value {1}.".FormatWith(first, second));
+        }
+
+        /// <summary>
+        /// Creates an <see cref="InclusiveInteger"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static InclusiveInteger Inclusive(this int value)
+        {
+            return new InclusiveInteger(value);
+        }
+
+        /// <summary>
+        /// Creates an <see cref="ExclusiveInteger"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static ExclusiveInteger Exclusive(this int value)
+        {
+            return new ExclusiveInteger(value);
+        }
+
+        /// <summary>
+        /// Determines whether the value is between the specified limits.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="lowerLimit">The lower limit.</param>
+        /// <param name="upperLimit">The upper limit.</param>
+        /// <returns>true is value is between lower and upper limit, otherwise false.</returns>
+        /// <exception cref="InvalidOperationException">When lowerLimit is greater than upperLimit.</exception>
+        public static bool IsBetween(this int value, ExclusiveInteger lowerLimit, ExclusiveInteger upperLimit)
+        {
+            EnsureOrder(lowerLimit.Value, upperLimit.Value);
+            return lowerLimit.Value < value && value < upperLimit.Value;
+        }
+
+        /// <summary>
+        /// Determines whether the value is between the specified limits.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="lowerLimit">The lower limit.</param>
+        /// <param name="upperLimit">The upper limit.</param>
+        /// <returns>true is value is between lower (inclusive) and upper limit, otherwise false.</returns>
+        /// <exception cref="InvalidOperationException">When lowerLimit is greater than upperLimit.</exception>
+        public static bool IsBetween(this int value, InclusiveInteger lowerLimit, ExclusiveInteger upperLimit)
+        {
+            EnsureOrder(lowerLimit.Value, upperLimit.Value);
+            return lowerLimit.Value <= value && value < upperLimit.Value;
+        }
+
+        /// <summary>
+        /// Determines whether the value is between the specified limits.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="lowerLimit">The lower limit.</param>
+        /// <param name="upperLimit">The upper limit.</param>
+        /// <returns>true is value is between lower and upper limit (inclusive), otherwise false.</returns>
+        /// <exception cref="InvalidOperationException">When lowerLimit is greater than upperLimit.</exception>
+        public static bool IsBetween(this int value, ExclusiveInteger lowerLimit, InclusiveInteger upperLimit)
+        {
+            EnsureOrder(lowerLimit.Value, upperLimit.Value);
+            return lowerLimit.Value < value && value <= upperLimit.Value;
+        }
+
+        /// <summary>
+        /// Determines whether the value is between the specified limits.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="lowerLimit">The lower limit.</param>
+        /// <param name="upperLimit">The upper limit.</param>
+        /// <returns>true is value is between lower (inclusive) and upper limit (inclusive), otherwise false.</returns>
+        /// <exception cref="InvalidOperationException">When lowerLimit is greater than upperLimit.</exception>
+        public static bool IsBetween(this int value, InclusiveInteger lowerLimit, InclusiveInteger upperLimit)
+        {
+            EnsureOrder(lowerLimit.Value, upperLimit.Value);
+            return lowerLimit.Value <= value && value <= upperLimit.Value;
+        }
+
+        /// <summary>
         /// Determines whether the value is inclusively between the lower and upper limit.
         /// </summary>
         /// <param name="value">The value.</param>
@@ -17,12 +104,12 @@ namespace BCLExtensions
         /// <exception cref="InvalidOperationException">When lowerLimit is greater than upperLimit.</exception>
         public static bool IsBetween(this int value, int lowerLimit, int upperLimit)
         {
-            if(upperLimit<lowerLimit)throw new InvalidOperationException("The upperLimit cannot be less than the lowerLimit.");
+            EnsureOrder(lowerLimit, upperLimit);
             return lowerLimit <= value && value <= upperLimit;
         }
 
         /// <summary>
-        /// Determines whether [is between exclusive] [the specified lower limit].
+        /// Determines whether the value is between the lower and upper limit (exclusive).
         /// </summary>
         /// <param name="value">The value.</param>
         /// <param name="lowerLimit">The lower limit.</param>
@@ -31,8 +118,9 @@ namespace BCLExtensions
         /// <exception cref="InvalidOperationException">When lowerLimit is greater than upperLimit.</exception>
         public static bool IsBetweenExclusive(this int value, int lowerLimit, int upperLimit)
         {
-            if (upperLimit < lowerLimit) throw new InvalidOperationException("The upperLimit cannot be less than the lowerLimit.");
+            EnsureOrder(lowerLimit, upperLimit);
             return lowerLimit < value && value < upperLimit;
         }
+
     }
 }
