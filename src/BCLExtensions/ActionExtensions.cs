667096667d63ff3d2549df753b83dd377d884613
Mark Clearwater <csmacnz@csmac.co.nz>
2015-04-11 15:32:45 +1200

    Added AsActionUsing for 6 and 7 args

--- src/BCLExtensions/ActionExtensions.cs
+++ src/BCLExtensions/ActionExtensions.cs
@@ -86,5 +86,51 @@ namespace BCLExtensions
         {
             return () => action(parameter1, parameter2, parameter3, parameter4, parameter5);
         }
+
+        /// <summary>
+        /// Extension method to expose a <see cref="System.Action{T1,T2,T3,T4,T5,T6}" /> as an  <see cref="System.Action" /> .
+        /// </summary>
+        /// <typeparam name="T1">The type of the 1.</typeparam>
+        /// <typeparam name="T2">The type of the 2.</typeparam>
+        /// <typeparam name="T3">The type of the 3.</typeparam>
+        /// <typeparam name="T4">The type of the 4.</typeparam>
+        /// <typeparam name="T5">The type of the 5.</typeparam>
+        /// <typeparam name="T6">The type of the 6.</typeparam>
+        /// <param name="action">The action.</param>
+        /// <param name="parameter1">The parameter1.</param>
+        /// <param name="parameter2">The parameter2.</param>
+        /// <param name="parameter3">The parameter3.</param>
+        /// <param name="parameter4">The parameter4.</param>
+        /// <param name="parameter5">The parameter5.</param>
+        /// <param name="parameter6">The parameter6.</param>
+        /// <returns></returns>
+        public static Action AsActionUsing<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T1 parameter1, T2 parameter2, T3 parameter3, T4 parameter4, T5 parameter5, T6 parameter6)
+        {
+            return () => action(parameter1, parameter2, parameter3, parameter4, parameter5, parameter6);
+        }
+
+        /// <summary>
+        /// Extension method to expose a <see cref="System.Action{T1,T2,T3,T4,T5,T6,T7}" /> as an  <see cref="System.Action" /> .
+        /// </summary>
+        /// <typeparam name="T1">The type of the 1.</typeparam>
+        /// <typeparam name="T2">The type of the 2.</typeparam>
+        /// <typeparam name="T3">The type of the 3.</typeparam>
+        /// <typeparam name="T4">The type of the 4.</typeparam>
+        /// <typeparam name="T5">The type of the 5.</typeparam>
+        /// <typeparam name="T6">The type of the 6.</typeparam>
+        /// <typeparam name="T7">The type of the 7.</typeparam>
+        /// <param name="action">The action.</param>
+        /// <param name="parameter1">The parameter1.</param>
+        /// <param name="parameter2">The parameter2.</param>
+        /// <param name="parameter3">The parameter3.</param>
+        /// <param name="parameter4">The parameter4.</param>
+        /// <param name="parameter5">The parameter5.</param>
+        /// <param name="parameter6">The parameter6.</param>
+        /// <param name="parameter7">The parameter7.</param>
+        /// <returns></returns>
+        public static Action AsActionUsing<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T1 parameter1, T2 parameter2, T3 parameter3, T4 parameter4, T5 parameter5, T6 parameter6, T7 parameter7)
+        {
+            return () => action(parameter1, parameter2, parameter3, parameter4, parameter5, parameter6, parameter7);
+        }
     }
 }
