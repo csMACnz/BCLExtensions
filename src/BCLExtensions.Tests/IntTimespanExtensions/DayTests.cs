b63444f55d5637108bd235aa4b4de28e45ae907a
Mark Clearwater <csmacnz@csmac.co.nz>
2015-06-09 18:29:32 +1200

    Added method to turn int into Timespan of Days

---
+++ src/BCLExtensions.Tests/IntTimespanExtensions/DayTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using Xunit.Extensions;
+
+namespace BCLExtensions.Tests.IntTimespanExtensions
+{
+    public class DayTests
+    {
+        [Fact]
+        public void WorksWhenUsedOnAnInlineConstant()
+        {
+            TimeSpan result = (1).Day();
+
+            Assert.Equal(1, result.TotalDays);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(265)]
+        [InlineData(9001)]
+        [InlineData(-1)]
+        [InlineData(-5)]
+        [InlineData(-10)]
+        [InlineData(-265)]
+        [InlineData(-9001)]
+        [InlineData(-10675199)]
+        [InlineData(10675199)]
+        public void WhenGivenANumberThenReturnsCorrectTimeSpan(int numberOfDays)
+        {
+            TimeSpan result = numberOfDays.Day();
+
+            Assert.Equal(numberOfDays, result.TotalDays);
+        }
+
+        [Theory]
+        [InlineData(-10675200)]
+        [InlineData(10675200)]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        public void DayCountsOutsideOfTheMaximumLimitThrowAnArgumentOutOfRangeException(int numberOfDays)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => numberOfDays.Day());
+        }
+    }
+}
---
+++ src/BCLExtensions/IntTimespanExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BCLExtensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="int"/> that return <see cref="TimeSpan"/> values.
+    /// </summary>
+    public static class IntTimespanExtensions
+    {
+        /// <summary>
+        /// The specified number in days as a <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="numberOfDays">The number of days.</param>
+        /// <returns>The <see cref="TimeSpan"/> representation.</returns>
+        public static TimeSpan Day(this int numberOfDays)
+        {
+            return Days(numberOfDays);
+        }
+
+        /// <summary>
+        /// The specified number in days as a <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="numberOfDays">The number of days.</param>
+        /// <returns>The <see cref="TimeSpan"/> representation.</returns>
+        public static TimeSpan Days(this int numberOfDays)
+        {
+            return new TimeSpan(numberOfDays, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// Determines whether the number of days is valid for time span.
+        /// </summary>
+        /// <param name="numberOfDays">The number of days.</param>
+        /// <returns>true if number is valid, otherwise false.</returns>
+        public static bool IsAValidNumberOfDaysForTimeSpan(this int numberOfDays)
+        {
+            return TimeSpan.MinValue.TotalDays <= numberOfDays && numberOfDays <= TimeSpan.MaxValue.TotalDays;
+        }
+    }
+}
