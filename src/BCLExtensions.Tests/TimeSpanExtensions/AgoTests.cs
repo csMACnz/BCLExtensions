931ae536d8006255a9e611fbaf30561bf99cc727
Mark Clearwater <csmacnz@csmac.co.nz>
2015-07-08 23:10:40 +1200

    Added Ago Extension for time

---
+++ src/BCLExtensions.Tests/TimeSpanExtensions/AgoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace BCLExtensions.Tests.TimeSpanExtensions
+{
+    public class AgoTests
+    {
+        [Fact]
+        public void WorksWhenUsedAsAnExtension()
+        {
+            TimeSpan duration = (2).Minutes();
+            DateTime result = duration.Ago();
+            var now = DateTime.Now;
+
+            var errorMargin = 2.Seconds();
+            var resultBelowUpperBound = result <= (now - duration + errorMargin);
+            var resultAboveLowerBound = result >= (now - duration) - errorMargin;
+            Assert.True(resultBelowUpperBound && resultAboveLowerBound);
+        }
+
+    }
+}
---
+++ src/BCLExtensions/TimeSpanExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BCLExtensions
+{
+    /// <summary>
+    /// Extension methods for the <see cref="TimeSpan"/> type.
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        /// <summary>
+        /// Substracts the specified duration from the current DateTime.Now value.
+        /// </summary>
+        /// <param name="duration">The duration.</param>
+        /// <returns>The calculated Result DateTime.</returns>
+        public static DateTime Ago(this TimeSpan duration)
+        {
+            return DateTime.Now - duration;
+        }
+    }
+}
