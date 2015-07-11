7f4fffd78969cfaafacf4e2eadc09d5a8e123a12
Mark Clearwater <csmacnz@csmac.co.nz>
2015-07-11 17:05:23 +1200

    Added Remaining TimeSpan Extensions

---
+++ src/BCLExtensions.Tests/TimeSpanExtensions/BeforeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BCLExtensions.Tests.TestHelpers;
+using Xunit;
+
+namespace BCLExtensions.Tests.TimeSpanExtensions
+{
+    public class BeforeTests
+    {
+        [Fact]
+        public void WorksWhenUsedAsAnExtension()
+        {
+            var now = DateTime.Now;
+            var result = 5.Minutes().Before(now);
+            Assert.Equal(now - TimeSpan.FromMinutes(5), result);
+        }
+
+        [Fact]
+        public void AttemptToGoBeforeMinValueCausesArgumentOutOfRangeException()
+        {
+            var startOfTime = DateTime.MinValue;
+            var twoMinutes = 2.Minutes();
+
+            Func<TimeSpan, DateTime, DateTime> beforeAction =
+                BCLExtensions.TimeSpanExtensions.Before;
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                beforeAction.AsActionUsing(twoMinutes, startOfTime).AsThrowsDelegate());
+        }
+    }
+}
--- src/BCLExtensions/TimeSpanExtensions.cs
+++ src/BCLExtensions/TimeSpanExtensions.cs
@@ -16,5 +16,39 @@ namespace BCLExtensions
         {
             return DateTime.Now - duration;
         }
+
+        /// <summary>
+        /// Adds the specified duration from the current DateTime.Now value.
+        /// </summary>
+        /// <param name="duration">The duration.</param>
+        /// <returns>The calculated Result DateTime.</returns>
+        public static DateTime FromNow(this TimeSpan duration)
+        {
+            return DateTime.Now + duration;
+        }
+
+        /// <summary>
+        /// Substracts the specified duration from the provided <see cref="System.DateTime" />.
+        /// </summary>
+        /// <param name="duration">The duration.</param>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns>The calculated Result DateTime.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static DateTime Before(this TimeSpan duration, DateTime dateTime)
+        {
+            return dateTime - duration;
+        }
+
+        /// <summary>
+        /// Adds the specified duration from the provided <see cref="System.DateTime" />.
+        /// </summary>
+        /// <param name="duration">The duration.</param>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns>The calculated Result DateTime.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static DateTime After(this TimeSpan duration, DateTime dateTime)
+        {
+            return dateTime + duration;
+        }
     }
 }
